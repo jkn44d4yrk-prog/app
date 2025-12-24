7c411fe2827731dbc1315f752a119de2e3536ef0
jkn44d4yrk-prog <jkn44d4yrk@privaterelay.appleid.com>
2025-12-24 23:27:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,12 @@ document.addEventListener("DOMContentLoaded", () => {
     blockMsgEl.style.display = "none";
     menuEl.style.display = "block";
 
+    // Verificamos si el contenedor existe
+    if (menuBlocksEl === null) {
+      console.error("El elemento menuBlocksEl no se encuentra.");
+      return;
+    }
+
     // Pintar bloques
     menuBlocksEl.innerHTML = "";
     const numBlocks = Math.ceil(questions.length / BLOCK_SIZE);
